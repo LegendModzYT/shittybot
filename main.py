4934598a76d8c0f88791880e45b6db3c8e8839fb
LegendModzYT <39273354+LegendModzYT@users.noreply.github.com>
2019-03-31 19:18:57 +0100

    Updated

--- main.py
+++ main.py
@@ -7,12 +7,46 @@ from keep_alive import keep_alive
 banned = ['pop', 'dö', 'auschwitz', 'fan', 'kaka', 'jesus', 'satan', 'aids', 'tjock', 'feting', 'tack', 'varsågod', 'neger', 'nibba', 'hoe',':hyperxd:', 'kalle', 'bög', 'Olouf ']
 
 words_re = re.compile("|".join(banned))
-client = discord.Client()
+client = commands.Bot(command_prefix=',')
+
+
 @client.event
 async def on_ready():
     print("I'm in")
     print(client.user)
-@client.event
+    
+ @client.command(pass_context=True)
+async def help(ctx):
+    author = ctx.message.author
+    
+    embed = discord.Embed(
+        colour = discord.Colour.green()
+    )
+    
+    embed.set_author(name='Help')
+    embed.add_field(name='.ping', value='Returns Pong!', inline=True)
+    embed.add_field(name='.clear (value)', value='The Value Is The Amount Of Messages You Want To Delete.', inline=True)
+    embed.add_field(name='.', value='', inline=True)
+    embed.add_field(name='., value='', inline=True)
+    
+    await client.say("I Have Sent You A DM With All Of The Information.")
+
+    await client.send_message(author, embed=embed)
+
+@client.command(pass_context=True)
+async def clear(ctx, amount=50):
+    if str(ctx.message.author.id) == '239719341958889473' :
+        channel = ctx.message.channel
+        messages = []
+        async for message in client.logs_from(channel, limit=int(amount)):
+         messages.append(message)
+        await client.say('Messages Deleted.')
+        await client.delete_messages(messages)
+    else:
+        userID = ctx.message.author.id
+        await client.say("<@%s> : ⛔️ | Bot Owner only!" % (userID))
+
+@client.commands
 async def on_message(message):
     print(message.author,": ", message.channel)
     print()
